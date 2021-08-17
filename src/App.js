2a0fa7f1ba8a7b3dd2c1213ec3d1b02cc4414d1f
iarecinos <47794098+iarecinos@users.noreply.github.com>
2021-08-16 23:48:31 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import './App.css';
 
 
 function App() {
-  const [unidad, setUnidad] = useState(0);
-  const [decena, setDecena] = useState(0);
-  const [centena, setCentena] = useState(0);
-  const [umil, setUmil] = useState(0);
-  const [dmil, setDmil] = useState(0);
-  const [cmil, setCmil] = useState(0);
+  const [dollar, setDollar] = useState(0);
+  const [euro, setEuro] = useState(0);
+  const [bitcoin, setBitcoin] = useState(0);
 
   const [tipo, setTipo] = useState(1);
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    setUnidad((total * tipo) / 1);
-    setDecena((total * tipo) / 10);
-    setCentena((total * tipo) / 100);
-    setUmil((total * tipo) / 1000);
-    setDmil((total * tipo) / 10000);
-    setCmil((total * tipo) / 100000);
+    setDollar((total * tipo) / 1);
+    setEuro((total * tipo) / 1.18);
+    setBitcoin((total * tipo) / 46423.50);
   }, [total, tipo]);
 
 
@@ -32,26 +26,20 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Convertidor de unidades</h1>
+      <h1>Convertidor de divisas</h1>
 
-      <p>Unidad: {unidad}</p>
-      <p>Decena: {decena}</p>
-      <p>Centena: {centena}</p>
-      <p>Unidad Mil: {umil}</p>
-      <p>Decena Mil: {dmil}</p>
-      <p>Centena Mil: {cmil}</p>
+      <p>Dollar: {dollar}</p>
+      <p>Euro: {euro}</p>
+      <p>Bitcoin: {bitcoin}</p>
 
       <hr />
 
       <h3>{total}</h3>
 
       <select onChange={event => setTipo(event.target.value)} value={tipo}>
-        <option value={1}>Unidad</option>
-        <option value={10}>Decena</option>
-        <option value={100}>Centena</option>
-        <option value={1000}>Unidad Mil</option>
-        <option value={10000}>Decena Mil</option>
-        <option value={100000}>Centena Mil</option>
+        <option value={1}>Dollar</option>
+        <option value={1.18}>Euro</option>
+        <option value={46423.50}>Bitcoin</option>
       </select>
       <input onChange={handleTotalChange} value={total} />
     </div>
